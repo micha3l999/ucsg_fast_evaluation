50312e8a76cb5ceb72548d5b5f8c5252a2afcb04
Michael Mora <maycolmora99@gmail.com>
2022-02-01 01:59:50 -0500

    modified: retrieve userId in all buildings page

---
+++ get-all-user-data/lambda_function.py
@@ -0,0 +1,39 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+
+    # Getting dynamodb resource
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('Buildings')
+
+    # Retrieving all information about buildings
+    scan_args = {
+        'ProjectionExpression': "id, userName, userId"
+    }
+
+    done = False
+    start_key = None
+    items = None
+    while not done:
+        if start_key:
+            scan_args['ExclusiveStartKey'] = start_key
+        response = table.scan(**scan_args)
+        items = response.get('Items', [])
+        start_key = response.get('LastEvaluatedKey', None)
+        done = start_key is None
+
+    # Response for the client
+    data = {
+        "message": "All data retrieved",
+        "buildings": items
+    }
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(data),
+        'headers': {
+            "Access-Control-Allow-Origin" : "*",
+            "Access-Control-Allow-Credentials" : True
+        }
+    }
